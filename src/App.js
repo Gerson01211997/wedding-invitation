bc476ca6df0167838ec71ef525d672773f064b46
Gerson01211997 <gersonhoyos_11@hotmail.com>
2025-10-01 23:52:06 -0500

    fix deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Hero from './components/Hero/Hero';
-import Family from './components/Family/Family';
+// import Family from './components/Family/Family';
 import Ceremony from './components/Ceremony/Ceremony';
 import Confirmation from './components/Confirmation/Confirmation';
 import DressCode from './components/DressCode/DressCode';
-import InspirationBoard from './components/InspirationBoard/InspirationBoard';
+// import InspirationBoard from './components/InspirationBoard/InspirationBoard';
 import FAQ from './components/FAQ/FAQ';
-import Footer from './components/Footer/Footer';
+// import Footer from './components/Footer/Footer';
 
 function App() {
   return (
